09c87e38feeb72d74d0a12fa24e1dea44c945110
Ramy Awad <ramyaqt20@gmail.com>
2021-08-15 14:07:34 +0200

    final commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const item = document.querySelectorAll(".menu__item");
+const menu = document.querySelectorAll(".menu__c");
+const active = document.querySelectorAll(".active");
+
+let getItem = (event) => {
+  removeMenu();
+  let indexItem = event.currentTarget.id;
+  indexItem = indexItem.split("i-").join("");
+  for (var i = 0; i < active.length; i++) {
+    active[i].style.left = `${indexItem * 60 + 15}px`;
+  }
+  event.currentTarget.children[0].classList.add("is-c-translate");
+  event.currentTarget.children[1].classList.add("is-c-translate");
+};
+
+let removeMenu = () => {
+  for (var i = 0; i < menu.length; i++) {
+    menu[i].classList.remove("is-c-translate");
+  }
+};
+/* */
+let mainFunc = () => {
+  for (var i = 0; i < item.length; i++) {
+    item[i].addEventListener("click", getItem);
+  }
+};
+window.addEventListener("load", mainFunc);
